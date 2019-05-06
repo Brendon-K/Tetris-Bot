0fb989541e3d0431e195731261565e584549bd25
Brendon-K <brendon.kieser@gmail.com>
2019-05-06 11:58:41 -0700

    calculates area and perimeter of the board

---
+++ calc.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+# constants because the tetris board is always 20x10
+ROWS = 20
+COLS = 10
+
+# get small board in grayscale
+img = cv2.imread('board.png', 0)
+area = 0
+perimeter = 0
+for i, row in enumerate(img) :
+	for j, col in enumerate(row) :
+		print(i, j)
+		if col == 255 : 
+			area += 1
+			if i > 0 and img[i-1][j] != 255 :
+				img[i-1][j] += 1
+				perimeter += 1
+			if i < ROWS-1 and img[i+1][j] != 255 :
+				img[i+1][j] += 1
+				perimeter += 1
+			if j > 0 and img[i][j-1] != 255 :
+				img[i][j-1] += 1
+				perimeter += 1
+			if j < COLS-1 and img[i][j+1] != 255 :
+				img[i][j+1] += 1
+				perimeter += 1
+
+print(img)
+print('Area:', area)
+print('Perimeter:', perimeter)
