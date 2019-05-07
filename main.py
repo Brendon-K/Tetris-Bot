7dc3291cee72f9de03cbaf0e37a90e887f8e8b0a
Brendon-K <brendon.kieser@gmail.com>
2019-05-07 06:06:01 -0700

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from ss import *
+from vis import *
+from calc import *
+
+# sleep gives enough time to switch to emulator
+# might include something later that auto switches if possible
+sleep(2)
+ss_board()
+ss_next()
+create_mini_board()
+create_mini_next()
+data()
