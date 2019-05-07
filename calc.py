4089c90c1abba4c1ea2fcf15610d5b16e43feac6
Brendon-K <brendon.kieser@gmail.com>
2019-05-07 06:03:44 -0700

    Put code in a function

--- calc.py
+++ calc.py
@@ -6,30 +6,30 @@ from time import sleep
 ROWS = 20
 COLS = 10
 
-# get small board in grayscale
-img = cv2.imread('board.png', 0)
-area = 0
-perimeter = 0
-for i, row in enumerate(img) :
-	if sum(row) < 255 : 
-		area = 0
-		perimeter = 0
-	for j, col in enumerate(row) :
-		if col == 255 : 
-			area += 1
-			if i > 0 and img[i-1][j] != 255 :
-				img[i-1][j] += 1
-				perimeter += 1
-			if i < ROWS-1 and img[i+1][j] != 255 :
-				img[i+1][j] += 1
-				perimeter += 1
-			if j > 0 and img[i][j-1] != 255 :
-				img[i][j-1] += 1
-				perimeter += 1
-			if j < COLS-1 and img[i][j+1] != 255 :
-				img[i][j+1] += 1
-				perimeter += 1
+def data() :
+	# get small board in grayscale
+	img = cv2.imread('board.png', 0)
+	area = 0
+	perimeter = 0
+	for i, row in enumerate(img) :
+		if sum(row) < 255 : 
+			area = 0
+			perimeter = 0
+		for j, col in enumerate(row) :
+			if col == 255 : 
+				area += 1
+				if i > 0 and img[i-1][j] != 255 :
+					img[i-1][j] += 1
+					perimeter += 1
+				if i < ROWS-1 and img[i+1][j] != 255 :
+					img[i+1][j] += 1
+					perimeter += 1
+				if j > 0 and img[i][j-1] != 255 :
+					img[i][j-1] += 1
+					perimeter += 1
+				if j < COLS-1 and img[i][j+1] != 255 :
+					img[i][j+1] += 1
+					perimeter += 1
 
-print(img)
-print('Area:', area)
-print('Perimeter:', perimeter)
+	print('Area:', area)
+	print('Perimeter:', perimeter)
